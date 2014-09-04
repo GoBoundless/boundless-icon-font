b63f26fa5eb255952ce698855fe44570949a932d
Joe Webb <jwebb@boundless.com>
2014-09-04 18:09:55 -0400

    Wrote custom output tmplate, Removed node dependency
    new template serves ie9+ only, but much cleaner and faster

--- update_scss.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-data = File.read("prepared/_boundless-icon-font-rails.scss") 
-data.gsub!('       font-url("boundless-icon-font/boundless-icon-font.svg#boundless-icon-font") format("svg");','       font-url("boundless-icon-font/boundless-icon-font.svg#boundless-icon-font") format("svg"),
-       asset-data-url("boundless-icon-font/boundless-icon-font.woff") format("woff");')
-File.open("prepared/_boundless-icon-font-rails.scss","w") do |file|
-  file.puts data
-end
--- update_www.sh
+++ update_www.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# make the new shit
-fontcustom compile ./svg
-
 # copy the font files over
 cp -rf prepared/boundless-icon-font/* ../www/vendor/assets/fonts/boundless-icon-font/
 
-# update the scss
-ruby update_scss.rb
-
-# copy the updated font file over
-cp -f prepared/_boundless-icon-font-rails.scss ../www/app/assets/stylesheets/global/common
+# copy the updated scss file over
+cp -f prepared/_boundless-icon-font.scss ../www/app/assets/stylesheets/global/common
